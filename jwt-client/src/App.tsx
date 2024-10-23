575ee78490b0eb6ecaf20d54d1ed08a58aad2362
dsoto30 <sotodaniel531@gmail.com>
2024-10-22 22:32:33 -0700

    Finished Login and Register component with form validation with react bootstrap and useState

--- jwt-client/src/App.tsx
+++ jwt-client/src/App.tsx
@@ -1,22 +1,18 @@
-
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from "./components/Home";
-
-
-
-
+import Profile from "./components/Profile";
 
 function App() {
-
-  return (
-    <Routes>
-      <Route path="/" element={ <Home /> }/>
-      <Route path="/login" element={ <Login /> }/>
-      <Route path="/register" element= {<Register />}/>
-    </Routes>)
-  
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+        </Routes>
+    );
 }
 
-export default App
+export default App;
