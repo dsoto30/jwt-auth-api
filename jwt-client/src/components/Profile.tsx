56aeab05d1809cc927cbf496c7a57f4a588e1db5
dsoto30 <sotodaniel531@gmail.com>
2024-12-08 19:20:34 -0800

    added logout functionality

--- jwt-client/src/components/Profile.tsx
+++ jwt-client/src/components/Profile.tsx
@@ -1,21 +1,47 @@
 import React from "react";
+import { Card, Container } from "react-bootstrap";
 import { useAuthContext } from "../hooks/authHooks";
 
 export default function Profile() {
     const {
         state: { user },
-        isAuthenticating,
     } = useAuthContext();
 
-    if (isAuthenticating) {
-        return <div>Loading...</div>;
-    }
+    const logout = () => {
+        const logoutProcess = async () => {
+            try {
+                await fetch("api/users/logout", {
+                    credentials: "include",
+                });
+                window.location.reload();
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        logoutProcess();
+    };
 
     return (
-        <div>
-            <h1>Profile: Welcome !</h1>
-            <p>{user ? user.email : "No user logged in"}</p>
-            <p>Your id is: {user ? user.id : "No user logged in"}</p>
-        </div>
+        <Container className="d-flex justify-content-center mt-5">
+            <Card style={{ width: "18rem" }} className="shadow">
+                <Card.Body>
+                    <Card.Title className="text-center">
+                        User Profile
+                    </Card.Title>
+                    <Card.Text>
+                        <strong>User ID:</strong> {user ? user.id : "Unknown"}
+                    </Card.Text>
+                    <Card.Text>
+                        <strong>Email:</strong> {user ? user.email : "Unknown"}
+                    </Card.Text>
+                    <button
+                        className="btn btn-primary rounded"
+                        onClick={logout}
+                    >
+                        Logout
+                    </button>
+                </Card.Body>
+            </Card>
+        </Container>
     );
 }
